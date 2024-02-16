4cef17b4afd62ef085e58540d9d4c830af17f629
Dhanush B <talktodhanush4554@gmail.com>
2024-02-16 19:06:03 +0530

    changes done

--- mail/mail.php
+++ mail/mail.php
@@ -14,19 +14,16 @@ Message: ".$message."
 
 ";
 
-$to = "chethanvhoypuc2@gmail.com"; // Change this to your email address
+$to = "chethanvhoypuc2@gmail.com"; 
 $mail_subject = "New Message";
 
-// Make sure to have proper headers for the email
 $headers = "From: $email \r\n";
 $headers .= "Reply-To: $email \r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-// Send the email
 $mail_sent = mail($to, $mail_subject, $email_message, $headers);
 
-// Check if the email was sent successfully
 if ($mail_sent) {
     header("Location: ../mail-success.html");
 } else {
